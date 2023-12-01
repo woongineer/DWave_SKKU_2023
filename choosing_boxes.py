2bed403b09fc58915867ca4866d31acb35e529ab
woongineer <jheo2807@gmail.com>
2023-12-02 03:10:08 +0900

    feat choosing boxes

---
+++ choosing_boxes.py
@@ -0,0 +1,70 @@
+# Copyright 2020 D-Wave Systems Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+## ------- import packages -------
+from dwave.system import DWaveSampler, EmbeddingComposite
+from dimod import BinaryQuadraticModel
+
+
+# TODO:  Add code here to define your BQM
+def get_bqm(S):
+    gamma = 20
+    bqm = BinaryQuadraticModel('BINARY')
+
+    x = ['box_17', 'box_21', 'box_19']
+
+    # Add BQM construction here
+    for i in range(len(S)):
+        bqm.add_variable(x[i], S[i])
+
+    bqm.add_linear_equality_constraint(
+        [(box, 1) for box in x],
+        lagrange_multiplier=gamma,
+        constant=-2)
+
+    return bqm
+
+
+# TODO:  Choose QPU parameters in the following function
+def run_on_qpu(bqm, sampler):
+    """Runs the BQM on the sampler provided.
+
+    Args:
+        bqm (BinaryQuadraticModel): a BQM for the problem;
+            variable names should be 'box_17', 'box_21', and 'box_19'
+        sampler (dimod.Sampler): a sampler that uses the QPU
+    """
+
+    numruns = 100  # update
+
+    sample_set = sampler.sample(bqm, num_reads=numruns,
+                                label='Training - Choosing Boxes')
+
+    return sample_set
+
+
+## ------- Main program -------
+if __name__ == "__main__":
+    S = [17, 21, 19]
+
+    bqm = get_bqm(S)
+
+    # TODO:  Write code to define your sampler
+    sampler = EmbeddingComposite(DWaveSampler())
+
+    # TODO:  Write code to run your problem
+    sampleset = run_on_qpu(bqm, sampler)
+
+    # TODO:  Write code to look at the solutions returned
+    print(sampleset)
